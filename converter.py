e1765f21cc7f9c633a44c054598e6e4563c4e62f
Moab Barbosa <moab414@gmail.com>
2021-05-29 07:32:54 -0300

    update

--- converter.py
+++ converter.py
@@ -1,4 +1,4 @@
-import os
+﻿import os
 import subprocess
 
 folder = input('Insira o caminho absoluto para a pasta com os arquivos para serem convertidos para mp3: ')
@@ -12,6 +12,6 @@ if not os.path.exists(f'{folder}/Convertido - MP3'):
 for index, music in enumerate(input_folder):
     music_no_ext = music.split('.')[0]
     print(f'Convertendo música {index + 1} de {len(input_folder)}')
-    subprocess.run(["./FFmpeg/bin/ffmpeg.exe",'-i', f'{folder}/{music}', f'{folder}/Convertido - MP3/{music_no_ext}.mp3', '-loglevel', 'quiet'])
+    subprocess.run(["ffmpeg",'-i', f'{folder}/{music}', f'{folder}/Convertido - MP3/{music_no_ext}.mp3', '-loglevel', 'quiet'])
 
 print('Tudo pronto!')
